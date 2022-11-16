c550146e9b9b69062ae881f6f6c9e297a6e5ae52
Francisco Martinez <fh.martinez1996@gmail.com>
2022-11-16 05:30:08 -0300

    FIX: removed confusing console log functions.

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -11,30 +11,21 @@ const ItemListContainer = ({ greeting }) => {
         if(category){
             getProductos("../json/productos.json")
             .then(products => {
-                console.log("entre en el if: " + category)
                 const productList = products.filter(prod => prod.idCategoria === parseInt(category))
-                console.log(productList)
                 const cardProductos = ItemList({ productList })
                 setProductos(cardProductos)
                 const newPath = generatePath("/category/:category", {category: productList[0].categoria})
-                console.log(newPath)
                 window.history.replaceState(null, '', newPath)
             })
         } else {
             getProductos("./json/productos.json")
             .then(productList => {
-                console.log("no entre en el if: " + category)
-                console.log(productList)
                 const cardProductos = ItemList({ productList })
                 setProductos(cardProductos)
             })
-        }
-
-        
+        }        
 
     }, [location]);
-
-    // location.history.replace(newPath);
 
     return (
         <div className="row justify-content-evenly py-3">
